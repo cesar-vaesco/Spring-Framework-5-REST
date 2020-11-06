df8e1e31955c029390146c65a41db77762cbbdd6
Cesar <cesarvargasescorcia@gmail.com>
2020-11-06 00:35:48 -0600

    CORE: Inicializando proyecto - conociendo spring core

---
+++ hello-word/src/test/java/com/vaescode/hello/HelloWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaescode.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
