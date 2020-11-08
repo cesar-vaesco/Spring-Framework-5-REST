cec3e33f64df6d08c328349ac2daf005e9ef2c8e
Cesar <cesarvargasescorcia@gmail.com>
2020-11-07 19:55:20 -0600

    CORE: (Scope) prototype - nueca instancia cada que se solicite

--- dependecy-inyection/src/main/java/com/vaescode/di/scopes/EjemploScopeService.java
+++ dependecy-inyection/src/main/java/com/vaescode/di/scopes/EjemploScopeService.java
@@ -1,8 +1,10 @@
 package com.vaescode.di.scopes;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope("prototype")
 public class EjemploScopeService {
 
 }
