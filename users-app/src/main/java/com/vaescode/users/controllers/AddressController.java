3132f06bde2cc92aa9dfea996e6f3360cfffacbd
Cesar <cesarvargasescorcia@gmail.com>
2020-12-02 17:05:14 -0600

    REST: Creación de endpoint para agregar direcciones

--- users-app/src/main/java/com/vaescode/users/controllers/AddressController.java
+++ users-app/src/main/java/com/vaescode/users/controllers/AddressController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +26,11 @@ public class AddressController {
 	public ResponseEntity<List<Address>> findAddressByProfileAndUserId(@PathVariable("userId") Integer userId,
 			@PathVariable("profileId") Integer profileId) {
 		return new ResponseEntity<List<Address>>(addressService.findAddressByProfileAndUserId(userId, profileId), HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Address> create(@PathVariable("userId") Integer userId,
+			@PathVariable("profileId") Integer profileId, @RequestBody Address address){
+		return new ResponseEntity<Address>(addressService.createAddress(userId, profileId, address), HttpStatus.CREATED);
 	}
 }
--- users-app/src/main/java/com/vaescode/users/services/AddressService.java
+++ users-app/src/main/java/com/vaescode/users/services/AddressService.java
@@ -1,10 +1,15 @@
 package com.vaescode.users.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
 import com.vaescode.users.entities.Address;
+import com.vaescode.users.entities.Profile;
 import com.vaescode.users.repositories.AddressRepository;
 import com.vaescode.users.repositories.ProfileRepository;
 
@@ -20,6 +25,20 @@ public class AddressService {
 	public List<Address> findAddressByProfileAndUserId(Integer userId, Integer profileId) {
 
 		return addressRepository.findByProfileId(userId, profileId);
+	}
+
+	public Address createAddress(Integer userId, Integer profileId, Address address) {
+		Optional<Profile> result = profileRepository.getByUserAndProfileId(userId, profileId);
+		if (result.isPresent()) {
+
+			address.setProfile(result.get());
+			return addressRepository.save(address);
+		} else {
+
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+					String.format("Profile %d and %d not found", profileId, userId));
+		}
+
 	}
 
 	/**
